c9863528cf88ef909cf039679b3f3ed6e3853902
Harley Temponi <harley.goncalves@grupoavu.com.br>
2025-11-10 17:59:43 -0300

    CRUD Scaffolding

--- SalesWebMVC/obj/Debug/net8.0/SalesWebMVC.AssemblyInfo.cs
+++ SalesWebMVC/obj/Debug/net8.0/SalesWebMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ebbee597eb930fa608c86e13669820805ecca28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f14c9fa5e8ffa1ba2fb70b2f5e3de48f783d3d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
