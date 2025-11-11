6b4ec1b6f7d3db1eebff414b070f9231088c2268
Harley Temponi <harley.goncalves@grupoavu.com.br>
2025-11-11 10:05:19 -0300

    Re-adding previously deleted and necessary files

---
+++ SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Plugins;
+
+namespace SalesWebMVC.Data
+{
+
+    public class SalesWebMVCContext : DbContext
+    {
+        public SalesWebMVCContext (DbContextOptions<SalesWebMVCContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<SalesWebMVC.Models.Department> Department { get; set; }
+        //public DbSet<Department> Department { get; set; }
+        //public DbSet<Seller> Seller { get; set; }
+        //public DbSet<SalesRecord> SalesRecord { get; set; }
+    }
+}
--- SalesWebMVC/obj/Debug/net8.0/SalesWebMVC.AssemblyInfo.cs
+++ SalesWebMVC/obj/Debug/net8.0/SalesWebMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f14c9fa5e8ffa1ba2fb70b2f5e3de48f783d3d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9863528cf88ef909cf039679b3f3ed6e3853902")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
