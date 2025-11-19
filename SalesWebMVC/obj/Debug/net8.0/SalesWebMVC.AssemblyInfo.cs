1d9c4a6ffd784f6d659fe4872711324609cc3bd7
Harley Temponi <harley.goncalves@grupoavu.com.br>
2025-11-19 09:56:09 -0300

    Another Commit test using secondary local branch.

--- SalesWebMVC/obj/Debug/net8.0/SalesWebMVC.AssemblyInfo.cs
+++ SalesWebMVC/obj/Debug/net8.0/SalesWebMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a9e19f6c4e729f784c60271be4a00d3a96c80b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd04074e41964c7083b027bba0bd2d367c08b535")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
